cc16a5851a564e36439b9175aa39c2051e0307b6
shawn <xw2002121@gmail.com>
2024-02-24 02:52:31 +0800

    Multithreading

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,7 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprint(pagetable_t pagetable){
+
+}
--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -7,72 +7,87 @@
 static int nthread = 1;
 static int round = 0;
 
+static __thread int thread_flag = 0;
+
 struct barrier {
-  pthread_mutex_t barrier_mutex;
-  pthread_cond_t barrier_cond;
-  int nthread;      // Number of threads that have reached this round of the barrier
-  int round;     // Barrier round
+    pthread_mutex_t barrier_mutex;
+    pthread_cond_t barrier_cond;
+    int nthread;      // Number of threads that have reached this round of the barrier
+    int round;     // Barrier round
+    int flag;
 } bstate;
 
 static void
-barrier_init(void)
-{
-  assert(pthread_mutex_init(&bstate.barrier_mutex, NULL) == 0);
-  assert(pthread_cond_init(&bstate.barrier_cond, NULL) == 0);
-  bstate.nthread = 0;
+barrier_init(void) {
+    assert(pthread_mutex_init(&bstate.barrier_mutex, NULL) == 0);
+    assert(pthread_cond_init(&bstate.barrier_cond, NULL) == 0);
+    bstate.round = 0;
+    bstate.nthread = 0;
+    bstate.flag = 0;
 }
 
-static void 
-barrier()
-{
-  // YOUR CODE HERE
-  //
-  // Block until all threads have called barrier() and
-  // then increment bstate.round.
-  //
-  
+static void
+barrier() {
+    // YOUR CODE HERE
+    //
+    // Block until all threads have called barrier() and
+    // then increment bstate.round.
+    //
+    pthread_mutex_lock(&bstate.barrier_mutex);
+    thread_flag = !thread_flag;
+    while (thread_flag == bstate.flag) {
+        pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+    }
+    int arrived = ++bstate.nthread;
+    if (arrived == nthread) {
+        bstate.round++;
+        bstate.flag = !bstate.flag;
+        bstate.nthread = 0;
+        pthread_cond_broadcast(&bstate.barrier_cond);
+    } else {
+        pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+    }
+    pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
-thread(void *xa)
-{
-  long n = (long) xa;
-  long delay;
-  int i;
+thread(void *xa) {
+    long n = (long) xa;
+    long delay;
+    int i;
 
-  for (i = 0; i < 20000; i++) {
-    int t = bstate.round;
-    assert (i == t);
-    barrier();
-    usleep(random() % 100);
-  }
+    for (i = 0; i < 20000; i++) {
+        int t = bstate.round;
+        assert(i == t);
+        barrier();
+        usleep(random() % 100);
+    }
 
-  return 0;
+    return 0;
 }
 
 int
-main(int argc, char *argv[])
-{
-  pthread_t *tha;
-  void *value;
-  long i;
-  double t1, t0;
+main(int argc, char *argv[]) {
+    pthread_t *tha;
+    void *value;
+    long i;
+    double t1, t0;
 
-  if (argc < 2) {
-    fprintf(stderr, "%s: %s nthread\n", argv[0], argv[0]);
-    exit(-1);
-  }
-  nthread = atoi(argv[1]);
-  tha = malloc(sizeof(pthread_t) * nthread);
-  srandom(0);
+    if (argc < 2) {
+        fprintf(stderr, "%s: %s nthread\n", argv[0], argv[0]);
+        exit(-1);
+    }
+    nthread = atoi(argv[1]);
+    tha = malloc(sizeof(pthread_t) * nthread);
+    srandom(0);
 
-  barrier_init();
+    barrier_init();
 
-  for(i = 0; i < nthread; i++) {
-    assert(pthread_create(&tha[i], NULL, thread, (void *) i) == 0);
-  }
-  for(i = 0; i < nthread; i++) {
-    assert(pthread_join(tha[i], &value) == 0);
-  }
-  printf("OK; passed\n");
+    for (i = 0; i < nthread; i++) {
+        assert(pthread_create(&tha[i], NULL, thread, (void *) i) == 0);
+    }
+    for (i = 0; i < nthread; i++) {
+        assert(pthread_join(tha[i], &value) == 0);
+    }
+    printf("OK; passed\n");
 }
