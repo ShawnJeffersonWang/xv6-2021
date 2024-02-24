4b6963a8afe8521054705a3259651e55b0b8aa6c
shawn <xw2002121@gmail.com>
2024-02-24 14:06:59 +0800

    Copy-on-Write

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -9,34 +9,38 @@
 #include "riscv.h"
 #include "defs.h"
 
+#define PA2INDEX(pa) (((uint64)pa)/PGSIZE)
+
+int cowcount[PHYSTOP / PGSIZE];
+
 void freerange(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
-                   // defined by kernel.ld.
+// defined by kernel.ld.
 
 struct run {
-  struct run *next;
+    struct run *next;
 };
 
 struct {
-  struct spinlock lock;
-  struct run *freelist;
+    struct spinlock lock;
+    struct run *freelist;
 } kmem;
 
 void
-kinit()
-{
-  initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+kinit() {
+    initlock(&kmem.lock, "kmem");
+    freerange(end, (void *) PHYSTOP);
 }
 
 void
-freerange(void *pa_start, void *pa_end)
-{
-  char *p;
-  p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
-    kfree(p);
+freerange(void *pa_start, void *pa_end) {
+    char *p;
+    p = (char *) PGROUNDUP((uint64) pa_start);
+    for (; p + PGSIZE <= (char *) pa_end; p += PGSIZE) {
+        cowcount[PA2INDEX(p)] = 1;
+        kfree(p);
+    }
 }
 
 // Free the page of physical memory pointed at by v,
@@ -44,39 +48,61 @@ freerange(void *pa_start, void *pa_end)
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
 void
-kfree(void *pa)
-{
-  struct run *r;
+kfree(void *pa) {
+    struct run *r;
 
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
-    panic("kfree");
+    if (((uint64) pa % PGSIZE) != 0 || (char *) pa < end || (uint64) pa >= PHYSTOP)
+        panic("kfree");
 
-  // Fill with junk to catch dangling refs.
-  memset(pa, 1, PGSIZE);
+    acquire(&kmem.lock);
+    int remain = --cowcount[PA2INDEX(pa)];
+    release(&kmem.lock);
 
-  r = (struct run*)pa;
+    if (remain > 0) {
+        return;
+    }
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+    // Fill with junk to catch dangling refs.
+    memset(pa, 1, PGSIZE);
+
+    r = (struct run *) pa;
+
+    acquire(&kmem.lock);
+    r->next = kmem.freelist;
+    kmem.freelist = r;
+    release(&kmem.lock);
 }
 
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
 // Returns 0 if the memory cannot be allocated.
 void *
-kalloc(void)
-{
-  struct run *r;
+kalloc(void) {
+    struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    acquire(&kmem.lock);
+    r = kmem.freelist;
+    if (r)
+        kmem.freelist = r->next;
+    release(&kmem.lock);
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+    if (r) {
+        memset((char *) r, 5, PGSIZE); // fill with junk
+        int idx = PA2INDEX(r);
+        if (cowcount[idx] != 0) {
+            panic("kalloc: cowcount[idx]!=0");
+        }
+        cowcount[idx] = 1;
+    }
+    return (void *) r;
+}
+
+// helper
+void adjustref(uint64 pa, int num) {
+    if (pa >= PHYSTOP) {
+        panic("addref: pa too big");
+    }
+    acquire(&kmem.lock);
+    cowcount[PA2INDEX(pa)] += num;
+    release(&kmem.lock);
 }
